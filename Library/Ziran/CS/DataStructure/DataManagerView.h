c3de4d39223b731bd50585f06a351827e281e6ea
CleftObsidian <walkpark@khu.ac.kr>
2023-06-08 00:40:24 +0900

    Initial commit

---
+++ Library/Ziran/CS/DataStructure/DataManagerView.h
@@ -0,0 +1,25 @@
+#include <Ziran/CS/DataStructure/DisjointRanges.h>
+#include <Ziran/CS/DataStructure/DataManager.h>
+
+#ifndef DATA_MANAGER_VIEW
+#define DATA_MANAGER_VIEW
+
+namespace ZIRAN {
+
+struct DataManagerView {
+    const DataManager& manager;
+    DisjointRanges ranges;
+    DataManagerView(const DataManager& manager)
+        : manager(manager)
+        , ranges{ { 0, manager.count } }
+    {
+    }
+
+    DataManagerView(const DataManager& manager, const DisjointRanges& ranges)
+        : manager(manager)
+        , ranges(ranges)
+    {
+    }
+};
+} // namespace ZIRAN
+#endif
